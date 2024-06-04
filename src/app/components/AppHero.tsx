404ace83e5ecf526f2f5e1c10aa3a3abce4cf9c0
porworr <118798288+porworr@users.noreply.github.com>
2024-06-04 14:24:11 +0700

    map and onClick

--- src/app/components/AppHero.tsx
+++ src/app/components/AppHero.tsx
@@ -5,9 +5,19 @@ export default function AppHero() {
     const myHero = <p>my hero</p>;
     const dateNow = new Date();
     const isShow = true;
+    const students = [
+        {id: 1, fullname: "John Doe"},
+        {id: 2, fullname: "Jane Doe"},
+    ]
 
   return (
     <>
+    <h1>students list</h1>
+    {
+        students.map((item, index) => {
+            return (<div key={item.id}> index = {index} id = {item.id} name: {item.fullname}</div>)
+        })
+    }
     {
         isShow && <h1>{title}</h1>
     }
--- src/app/components/AppLogo.tsx
+++ src/app/components/AppLogo.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 export default function AppLogo() {
+    const handleClickMe = () => {
+        alert("Hello click event");
+    }
     return (
         <p>
           App Logo
+          <button onClick={handleClickMe}>Click me</button>
         </p>
     )
 }
