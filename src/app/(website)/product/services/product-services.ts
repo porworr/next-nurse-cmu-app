1da08212434075721e2decf45926147879a9df66
porworr <118798288+porworr@users.noreply.github.com>
2024-06-07 09:06:31 +0700

    Prisma

--- src/app/(website)/product/services/product-services.ts
+++ src/app/(website)/product/services/product-services.ts
@@ -3,7 +3,7 @@ import { notFound, redirect } from "next/navigation";
 export async function getProducts() {
     // const res = await fetch("https://api.codingthailand.com/api/course"); // static redering (cache ตลอดไป)
     // const res = await fetch("https://api.codingthailand.com/api/course", {next: {revalidate: 60}}); // ตั้ง cache ไว้ 60 วินาที
-    const res = await fetch("https://api.codingthailand.com/api/course999", {cache: "no-store"}); // ไม่ cache
+    const res = await fetch("https://api.codingthailand.com/api/course", {cache: "no-store"}); // ไม่ cache
     if (!res.ok) {
         // redirect("/");
         // notFound();
--- src/app/(website)/student/page.tsx
+++ src/app/(website)/student/page.tsx
@@ -1,9 +1,30 @@
-import { Container, Typography } from "@mui/material";
+import { prisma } from "@/lib/db";
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
 
-export default function StudentPage() {
+export default async function StudentPage() {
+  const students = await prisma.students.findMany({
+    // include: { Departments: true },
+    include: { Department: { select: { Name: true} } },
+    orderBy: { Id: "desc" }
+  });
+
   return (
     <Container>
-      <Typography variant="h1">Student Page</Typography>
+      {
+        students.length > 0 && (
+          students.map((item) => {
+            return (
+              <>
+                <Typography key={item.Id.toString()}> 
+                  {item.Id.toString()} {item.fname} {item.tel} 
+                  {item.Department?.Name}
+                </Typography>
+              </>
+            )
+          })
+        )
+      }
     </Container>
   );
 }
